576eea7f31865665a597fee14cd4097c9bb9b2a0
Sharungarg <sharungarg@gmail.com>
2018-05-24 16:56:07 +0530

    Update ProxyDesignPattern.java

--- ProxyDesignPattern.java
+++ ProxyDesignPattern.java
@@ -14,24 +14,38 @@ public class OpenInternet implements Internet{
 import java.util.List;
 import java.util.LinkedList;
 
-public class ProxyInternet implements Internet{
-	
-	//open access object of open internet
+package com.sharun.Internet;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProxyInternet implements Internet {
+
+	// open access object of open internet
 	Internet open_access = new OpenInternet();
-	
+
 	static List<String> banned_website;
-	static{
+
+	static {
 		banned_website = new LinkedList<String>();
 		banned_website.add("facebook.com");
 		banned_website.add("whatsapp.com");
 		banned_website.add("instagram.com");
 	}
-	public void connectTo(String website){
-		if(banned_website.contains(website))
-			System.out.println("You don't have access to " + website);
-			
-		else
+
+	public void connectTo(String website) {
+		if (banned_website.contains(website)) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter the password");
+			int password = sc.nextInt();
+			if (password == 123)
+				open_access.connectTo(website);
+			else
+				System.out.println("You don't have access to " + website);
+		} else {
 			open_access.connectTo(website);
+		}
 	}
 }
 
